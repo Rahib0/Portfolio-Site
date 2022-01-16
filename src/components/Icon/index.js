6360755454d649cc854da03e2fe57221a9e6e91c
Rahib <rrahman829@outlook.com>
2022-01-16 13:06:13 +0000

    changes to background on icon list

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -4,24 +4,30 @@ import './style.css'
 
 export default function Icon({ icon }) {
     const [ dancing, setDancing ] = useState("")
+    const [ selected, setSelected ] = useState("")
     const darkTheme = useTheme()
     
     const iconName = icon[0].replace(/\.[^/.]+$/, "")
     const iconPath = icon[1]
     let className = `icon_img${dancing}`
 
-    function handleMouseOver() {
+    function handleIconClick() {
         if (dancing) {
             setDancing("")
         } else {
             setDancing(" dancing")
         }
+        if (selected) {
+            setSelected("")
+        } else {
+            setSelected(" selected")
+        }
     }
 
     
     return (
-        <div className={`icon_container${darkTheme}`}>
-            <img src={iconPath} title={iconName} alt={iconName} onClick={handleMouseOver} className={className}></img>
+        <div className={`icon_container${darkTheme + selected}`}>
+            <img src={iconPath} title={iconName} alt={iconName} onClick={handleIconClick} className={className}></img>
         </div>
     )
 }
