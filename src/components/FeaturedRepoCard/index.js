e4ddbffaeeeb3c6e3d95c821772bc0445cb1f582
Rahib <rrahman829@outlook.com>
2022-04-18 13:18:06 +0100

    added show more button to repo card

--- src/components/FeaturedRepoCard/index.js
+++ src/components/FeaturedRepoCard/index.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTheme } from '../../ThemeContext'
 import './style.css'
 
 export default function FeaturedRepoCard({ repoData }) {
     const darkTheme = useTheme()
+    const [ isExpanded, setIsExpanded ] = useState(false)
 
     const openInNewTab = (url) => {
         const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
         if (newWindow) newWindow.opener = null
     }
 
+    function buttClick(){
+        if(isExpanded) {
+            setIsExpanded(false)
+        } else {
+            setIsExpanded(true)
+        }
+    }
+
     return (
         <div className='featured_card'>
             <h3 className={`featured_card_name${darkTheme}`}>{repoData.name}</h3>
             <img src={repoData.screenshot} className='screenshot' alt={repoData.name}></img>
-            <p className={`desc${darkTheme}`}>{repoData.desc}</p>
+            {/* <p className={`desc${darkTheme}`}>{repoData.desc}</p> */}
+            <div>
+                { isExpanded ? 
+                <div>
+                    <p>Placeholder</p>
+                    <button onClick={buttClick}>show less</button>
+                </div>:
+                <div>
+                    <p className={`desc${darkTheme}`}>{repoData.desc}</p> 
+                    <button onClick={buttClick}>show more</button>
+                </div> 
+                }
+                
+            </div>
+            
             <div className='card_butt_container'>
                 <button className={`card_butt github${darkTheme}`} onClick={() => openInNewTab(repoData.gitUrl)}>Github</button>
                 {repoData.liveUrl && <button className={`card_butt live${darkTheme}`} onClick={() => openInNewTab(repoData.liveUrl)}>Live Website</button>}
